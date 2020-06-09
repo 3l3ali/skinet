82318e70c4986b8b460320f1d0c1e0c1d8345060
Mohammad Alali <moe3l3ali@gmail.com>
2020-06-09 21:13:57 +0300

    fixed a double http request bug

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -61,11 +61,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number){
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number){
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -75,12 +77,16 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChange(event: any){
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if(this.shopParams.pageNumber !== event) // changing the page number applies onPageChange by default in pager-component
+    {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch(){
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
