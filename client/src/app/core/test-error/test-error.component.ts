ec4b1b853e1bb54de910383fcfa2647bf7426481
Mohammad Alali <moe3l3ali@gmail.com>
2020-06-17 03:51:26 +0300

    getting the errors list for 400 validation erros

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent implements OnInit {
   baseUrl: string = environment.apiUrl;
+  validationErrors: any;
 
   constructor(private http: HttpClient) { }
 
@@ -42,6 +43,7 @@ export class TestErrorComponent implements OnInit {
       console.log(response);
     }, error => {
       console.log(error);
+      this.validationErrors = error.errors;
     });
   }
 }
